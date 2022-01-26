c661b05f8ff34c790d4887bbb5f57d1a4c758f38
jimmiS2 <gyfl23242@naver.com>
2022-01-26 15:06:28 +0900

    바꾸기 전에 커밋한번 해두겠슴...

--- app/src/main/java/com/example/la/LearningActivity.java
+++ app/src/main/java/com/example/la/LearningActivity.java
@@ -4,18 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.dynamicanimation.animation.DynamicAnimation;
 import androidx.dynamicanimation.animation.FlingAnimation;
 
+import android.content.Context;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
+import android.util.AttributeSet;
 import android.util.Log;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.AbsListView;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ScrollView;
+
+import java.io.SequenceInputStream;
+import java.text.AttributedCharacterIterator;
 
 public class LearningActivity extends AppCompatActivity {
 
     private static final String TAG = "codingrun.StateChange";
     int[] ImageId = {R.drawable.volume, R.drawable.mute};
     ImageView MuteBtn;
+    ScrollView Scroll;
+    Button Day1;
+    Button Day4;
     int x, y;
 
 
@@ -28,11 +40,24 @@ public class LearningActivity extends AppCompatActivity {
         Log.i(TAG, "onCreate");
 
 
-        MuteBtn = (ImageView) findViewById(R.id.voice);
+        MuteBtn = (ImageView)findViewById(R.id.voice);
+        Scroll = (ScrollView)findViewById(R.id.scroll);
+        Day1 = (Button)findViewById(R.id.day1);
+        Day4 = (Button)findViewById(R.id.day4);
+
+
 
         MuteBtn.setImageResource(R.drawable.volume);
 
+
         MuteBtn.setOnClickListener(new LearningActivity.ChangeListener());
+
+        Day1.setOnClickListener(new Button.OnClickListener() {
+            @Override
+            public void onClick(View view){
+                Scroll.scrollTo(x = 0, y = Day4.getBottom());
+            }
+        });
 
 
 
@@ -43,6 +68,48 @@ public class LearningActivity extends AppCompatActivity {
         super.onStart();
         Log.i(TAG, "onCreate");
     }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.i(TAG, "onResume");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.i(TAG, "onPause");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.i(TAG, "onStop");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.i(TAG, "onRestart");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.i(TAG, "onDestroy");
+    }
+
+    /*
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        Log.i(TAG, "onSaveInstanceState");
+
+        final ScrollView Scroll = (ScrollView) findViewById(R.id.scroll);
+        SequenceInputStream S_Data =
+    }
+
+     */
 
     class ChangeListener implements View.OnClickListener {
         int i = 0;
@@ -56,4 +123,27 @@ public class LearningActivity extends AppCompatActivity {
 
         }//end onclick
     }//end ChangeListener()
+
+    class cal_distance
+
+    /*
+    internal fun ScrollView.computeDistanceToView(view: View): Int {
+        return abs(calculateRectOnScreen(this).top - (this.scrollY + calculateRectOnScreen(view).top))
+    }
+
+    internal fun calculateRectOnScreen(view: View): Rect {
+        val location = IntArray(2)
+        view.getLocationOnScreen(location)
+        return Rect(
+                location[0],
+                location[1],
+                location[0] + view.measuredWidth,
+                location[1] + view.measuredHeight
+        )
+    }
+
+     */
+
 }
+
+
